e25d098fb9251183d2aa912e5fa7430af1e47e98
Shiva Technic World <77491431+shivatechnicworld@users.noreply.github.com>
2022-01-21 19:20:29 +0530

    Add files via upload

---
+++ Shiva Technic World/app/src/main/java/APP/ShivaTechnicWorld/MoreActivity.java
@@ -0,0 +1,228 @@
+package BDNOOBRA.ShivaTechnicWorld;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.*;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import android.widget.LinearLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.DialogFragment;
+
+
+public class MoreActivity extends  AppCompatActivity  { 
+	
+	
+	private LinearLayout linear1;
+	private LinearLayout fb;
+	private LinearLayout linear4;
+	private LinearLayout yt;
+	private LinearLayout linear6;
+	private LinearLayout it;
+	private LinearLayout linear8;
+	private LinearLayout tg;
+	private LinearLayout linear10;
+	private LinearLayout tt;
+	private ImageView imageview1;
+	private TextView textview1;
+	private ImageView imageview2;
+	private TextView textview2;
+	private ImageView imageview3;
+	private TextView textview3;
+	private ImageView imageview4;
+	private TextView textview4;
+	private ImageView imageview5;
+	private TextView textview5;
+	
+	private Intent intent = new Intent();
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.more);
+		initialize(_savedInstanceState);
+		com.google.firebase.FirebaseApp.initializeApp(this);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		linear1 = (LinearLayout) findViewById(R.id.linear1);
+		fb = (LinearLayout) findViewById(R.id.fb);
+		linear4 = (LinearLayout) findViewById(R.id.linear4);
+		yt = (LinearLayout) findViewById(R.id.yt);
+		linear6 = (LinearLayout) findViewById(R.id.linear6);
+		it = (LinearLayout) findViewById(R.id.it);
+		linear8 = (LinearLayout) findViewById(R.id.linear8);
+		tg = (LinearLayout) findViewById(R.id.tg);
+		linear10 = (LinearLayout) findViewById(R.id.linear10);
+		tt = (LinearLayout) findViewById(R.id.tt);
+		imageview1 = (ImageView) findViewById(R.id.imageview1);
+		textview1 = (TextView) findViewById(R.id.textview1);
+		imageview2 = (ImageView) findViewById(R.id.imageview2);
+		textview2 = (TextView) findViewById(R.id.textview2);
+		imageview3 = (ImageView) findViewById(R.id.imageview3);
+		textview3 = (TextView) findViewById(R.id.textview3);
+		imageview4 = (ImageView) findViewById(R.id.imageview4);
+		textview4 = (TextView) findViewById(R.id.textview4);
+		imageview5 = (ImageView) findViewById(R.id.imageview5);
+		textview5 = (TextView) findViewById(R.id.textview5);
+		
+		fb.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				intent.setAction(Intent.ACTION_VIEW);
+				intent.setData(Uri.parse("https://www.facebook.com/shivatechnicworld1/"));
+				startActivity(intent);
+			}
+		});
+		
+		yt.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				intent.setAction(Intent.ACTION_VIEW);
+				intent.setData(Uri.parse("https://www.youtube.com/c/ShivaTechnicWorld/"));
+				startActivity(intent);
+			}
+		});
+		
+		it.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				intent.setAction(Intent.ACTION_VIEW);
+				intent.setData(Uri.parse("https://www.instagram.com/shivatechnicworld/"));
+				startActivity(intent);
+			}
+		});
+		
+		tg.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				intent.setAction(Intent.ACTION_VIEW);
+				intent.setData(Uri.parse("https://telegram.me/shivatechnicworld/"));
+				startActivity(intent);
+			}
+		});
+		
+		tt.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				intent.setAction(Intent.ACTION_VIEW);
+				intent.setData(Uri.parse("https://www.twitter.com/shivatechnicwo1/"));
+				startActivity(intent);
+			}
+		});
+	}
+	
+	private void initializeLogic() {
+		_rippleRoundStroke(fb, "#E0E0E0", "#000000", 6, 1, "#82B1FF");
+		_rippleRoundStroke(yt, "#E0E0E0", "#000000", 6, 1, "#82B1FF");
+		_rippleRoundStroke(it, "#E0E0E0", "#000000", 6, 1, "#82B1FF");
+		_rippleRoundStroke(tg, "#E0E0E0", "#000000", 6, 1, "#82B1FF");
+		_rippleRoundStroke(tt, "#E0E0E0", "#000000", 6, 1, "#82B1FF");
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			
+			default:
+			break;
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		intent.setClass(getApplicationContext(), HomeActivity.class);
+		startActivity(intent);
+		finish();
+	}
+	public void _rippleRoundStroke (final View _view, final String _focus, final String _pressed, final double _round, final double _stroke, final String _strokeclr) {
+		android.graphics.drawable.GradientDrawable GG = new android.graphics.drawable.GradientDrawable();
+		GG.setColor(Color.parseColor(_focus));
+		GG.setCornerRadius((float)_round);
+		GG.setStroke((int) _stroke,
+		Color.parseColor("#" + _strokeclr.replace("#", "")));
+		android.graphics.drawable.RippleDrawable RE = new android.graphics.drawable.RippleDrawable(new android.content.res.ColorStateList(new int[][]{new int[]{}}, new int[]{ Color.parseColor(_pressed)}), GG, null);
+		_view.setBackground(RE);
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input){
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels(){
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels(){
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+	
+}
---
+++ Shiva Technic World/app/src/main/java/APP/ShivaTechnicWorld/UpdateActivity.java
@@ -0,0 +1,264 @@
+package BDNOOBRA.ShivaTechnicWorld;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.*;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import java.util.HashMap;
+import android.widget.LinearLayout;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.HorizontalScrollView;
+import android.widget.TextView;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ChildEventListener;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import com.bumptech.glide.Glide;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.DialogFragment;
+
+
+public class UpdateActivity extends  AppCompatActivity  { 
+	
+	private Timer _timer = new Timer();
+	private FirebaseDatabase _firebase = FirebaseDatabase.getInstance();
+	
+	private HashMap<String, Object> map = new HashMap<>();
+	private String url = "";
+	private String link = "";
+	
+	private LinearLayout linear1;
+	private ImageView imageview1;
+	private ScrollView vscroll1;
+	private LinearLayout linear4;
+	private LinearLayout linear2;
+	private LinearLayout linear3;
+	private HorizontalScrollView hscroll1;
+	private TextView textview1;
+	private TextView whats_new;
+	private LinearLayout linear7;
+	private LinearLayout linear6;
+	private LinearLayout linear8;
+	private LinearLayout linear9;
+	private ImageView imageview2;
+	private ImageView imageview3;
+	private ImageView imageview4;
+	private LinearLayout linear5;
+	private TextView textview3;
+	
+	private DatabaseReference updatev1 = _firebase.getReference("update v1");
+	private ChildEventListener _updatev1_child_listener;
+	private TimerTask t;
+	private Intent i = new Intent();
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.update);
+		initialize(_savedInstanceState);
+		com.google.firebase.FirebaseApp.initializeApp(this);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		linear1 = (LinearLayout) findViewById(R.id.linear1);
+		imageview1 = (ImageView) findViewById(R.id.imageview1);
+		vscroll1 = (ScrollView) findViewById(R.id.vscroll1);
+		linear4 = (LinearLayout) findViewById(R.id.linear4);
+		linear2 = (LinearLayout) findViewById(R.id.linear2);
+		linear3 = (LinearLayout) findViewById(R.id.linear3);
+		hscroll1 = (HorizontalScrollView) findViewById(R.id.hscroll1);
+		textview1 = (TextView) findViewById(R.id.textview1);
+		whats_new = (TextView) findViewById(R.id.whats_new);
+		linear7 = (LinearLayout) findViewById(R.id.linear7);
+		linear6 = (LinearLayout) findViewById(R.id.linear6);
+		linear8 = (LinearLayout) findViewById(R.id.linear8);
+		linear9 = (LinearLayout) findViewById(R.id.linear9);
+		imageview2 = (ImageView) findViewById(R.id.imageview2);
+		imageview3 = (ImageView) findViewById(R.id.imageview3);
+		imageview4 = (ImageView) findViewById(R.id.imageview4);
+		linear5 = (LinearLayout) findViewById(R.id.linear5);
+		textview3 = (TextView) findViewById(R.id.textview3);
+		
+		linear5.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				i.setAction(Intent.ACTION_VIEW);
+				i.setData(Uri.parse(link));
+				startActivity(i);
+			}
+		});
+		
+		_updatev1_child_listener = new ChildEventListener() {
+			@Override
+			public void onChildAdded(DataSnapshot _param1, String _param2) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				whats_new.setText(_childValue.get("what's new").toString());
+				link = _childValue.get("new version link").toString();
+				t = new TimerTask() {
+					@Override
+					public void run() {
+						runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								
+								url = _childValue.get("ss1").toString();
+								Glide.with(getApplicationContext()).load(Uri.parse(url)).placeholder(R.drawable.ic_loading_png).into(imageview2);
+								
+								url = _childValue.get("ss2").toString();
+								Glide.with(getApplicationContext()).load(Uri.parse(url)).placeholder(R.drawable.ic_loading_png).into(imageview3);
+								
+								url = _childValue.get("ss3").toString();
+								Glide.with(getApplicationContext()).load(Uri.parse(url)).placeholder(R.drawable.ic_loading_png).into(imageview4);
+							}
+						});
+					}
+				};
+				_timer.schedule(t, (int)(1000));
+			}
+			
+			@Override
+			public void onChildChanged(DataSnapshot _param1, String _param2) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				
+			}
+			
+			@Override
+			public void onChildMoved(DataSnapshot _param1, String _param2) {
+				
+			}
+			
+			@Override
+			public void onChildRemoved(DataSnapshot _param1) {
+				GenericTypeIndicator<HashMap<String, Object>> _ind = new GenericTypeIndicator<HashMap<String, Object>>() {};
+				final String _childKey = _param1.getKey();
+				final HashMap<String, Object> _childValue = _param1.getValue(_ind);
+				
+			}
+			
+			@Override
+			public void onCancelled(DatabaseError _param1) {
+				final int _errorCode = _param1.getCode();
+				final String _errorMessage = _param1.getMessage();
+				
+			}
+		};
+		updatev1.addChildEventListener(_updatev1_child_listener);
+	}
+	
+	private void initializeLogic() {
+		_rippleRoundStroke(linear5, "#82B1FF", "#000000", 7, 1, "#000000");
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			
+			default:
+			break;
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		finishAffinity();
+	}
+	public void _rippleRoundStroke (final View _view, final String _focus, final String _pressed, final double _round, final double _stroke, final String _strokeclr) {
+		android.graphics.drawable.GradientDrawable GG = new android.graphics.drawable.GradientDrawable();
+		GG.setColor(Color.parseColor(_focus));
+		GG.setCornerRadius((float)_round);
+		GG.setStroke((int) _stroke,
+		Color.parseColor("#" + _strokeclr.replace("#", "")));
+		android.graphics.drawable.RippleDrawable RE = new android.graphics.drawable.RippleDrawable(new android.content.res.ColorStateList(new int[][]{new int[]{}}, new int[]{ Color.parseColor(_pressed)}), GG, null);
+		_view.setBackground(RE);
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input){
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels(){
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels(){
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+	
+}
